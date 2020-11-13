347295b6ce1e26b51e389e31d869b9885cd2122d
Joao Paulo Carvalho <labdev@USS-ENTERPRISE.localdomain>
2020-11-12 22:22:25 -0300

    Server config

---
+++ admin.js
@@ -0,0 +1,20 @@
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+
+const adminBro = new AdminBro({
+
+    database:[],
+    rootPath:'/admin',
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(adminBro.options.rootPath, router)
+
+app.listen(port, () => {
+  console.log(`Servidor executando http://localhost:${port}`)
+})
