2370d5b5959958392266552aae66cc86fecc527c
Joao Paulo Carvalho <labdev@USS-ENTERPRISE.localdomain>
2020-11-15 23:24:06 -0300

    Update admin.js, DB connect, Schema and resouces

--- admin.js
+++ admin.js
@@ -1,20 +1,67 @@
-const AdminBro = require('admin-bro')
-const AdminBroExpress = require('@admin-bro/express')
-
-const adminBro = new AdminBro({
-
-    database:[],
-    rootPath:'/admin',
-})
-
-const router = AdminBroExpress.buildRouter(adminBro)
-
+const mongoose = require('mongoose')
 const express = require('express')
-const app = express()
-const port = 3000
+const AdminBro = require('admin-bro')
 
-app.use(adminBro.options.rootPath, router)
+const ProjectSchema = new mongoose.Schema({
 
-app.listen(port, () => {
-  console.log('Servidor executando')
+title:{
+type:String,
+require: true,
+},
+
+description: String,
+completed: Boolean,
+created_at: { type: Date, default: Date.now},
+
 })
+
+const Project = mongoose.model("Project", ProjectSchema)
+
+
+const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
+
+AdminBro.registerAdapter(AdminBroMongoose)
+
+const adminBroOption = new AdminBro({
+	resources: [
+    { resource: Project, options: {
+      properties: {
+        description: { type: 'richtext' },
+        created_at: {
+          isVisible: { edit: false, list: true, show: true, filter: true }
+        }
+      }
+   }},
+  ],
+  locale: {
+    translations: {
+      labels: {
+        Project: 'Meus Projetos'
+      }
+    }
+  },
+  rootPath: '/admin'
+})
+
+const router = AdminBroExpress.buildRouter(adminBroOption)
+
+// Server
+const server = express();
+
+server
+  .use(adminBroOption.options.rootPath, router)
+
+const run = async () => {
+
+  mongoose.connect('mongodb://localhost/admin', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  
+  await server.listen(3000, () => console.log("Servidor executando"))
+  
+  }
+  
+  run()
